d7f499f7f62ae111798c323fafc02a776eb3a567
Holger Hetterich <hhetter@novell.com>
2010-11-30 18:15:55 +0100

    define a replacement for bzero. (bnc#654729).

--- src/network.c
+++ src/network.c
@@ -19,7 +19,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 #include "../include/includes.h"
-
+#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
 /**
  * receive a data block. return 0 if the connection
  * was closed, or return the received buffer
